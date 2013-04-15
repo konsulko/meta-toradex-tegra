0b47b985a4a9eb3449c7f3b0f0645ed5ca351da9
Max Krummenacher <max.krummenacher@toradex.com>
2013-04-15 12:55:48 +0200

    Apalis: make the lvds configuration glitch free

--- recipes-bsp/lvds-ctrl/files/lvds-dual-channel.sh
+++ recipes-bsp/lvds-ctrl/files/lvds-dual-channel.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 #enable the parallel RGB to LVDS controller in dual channel mode
 
-echo out > /sys/class/gpio/gpio216/direction
-echo 0 > /sys/class/gpio/gpio216/value
-echo out > /sys/class/gpio/gpio219/direction
-echo 1 > /sys/class/gpio/gpio219/value
-echo out > /sys/class/gpio/gpio220/direction
-echo 1 > /sys/class/gpio/gpio220/value
-echo out > /sys/class/gpio/gpio221/direction
-echo 1 > /sys/class/gpio/gpio221/value
-echo out > /sys/class/gpio/gpio222/direction
-echo 1 > /sys/class/gpio/gpio222/value
-echo out > /sys/class/gpio/gpio223/direction
-echo 1 > /sys/class/gpio/gpio223/value
-echo out > /sys/class/gpio/gpio225/direction
-echo 1 > /sys/class/gpio/gpio225/value
-echo out > /sys/class/gpio/gpio226/direction
-echo 1 > /sys/class/gpio/gpio226/value
+echo low > /sys/class/gpio/gpio216/direction
+echo hi  > /sys/class/gpio/gpio219/direction
+echo hi  > /sys/class/gpio/gpio222/direction
+echo hi  > /sys/class/gpio/gpio223/direction
+echo hi  > /sys/class/gpio/gpio226/direction
+echo hi  > /sys/class/gpio/gpio225/direction
+echo hi  > /sys/class/gpio/gpio221/direction
+echo hi  > /sys/class/gpio/gpio220/direction
